8b94ab2532d5ae88ee3ae8dbbed04b74563a26ec
moxun33 <mxx0807@gmail.com>
2022-07-04 17:18:51 +0800

    fix: pnpm

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.less';
 import { ConfigProvider } from 'antd';
-import resso from 'resso';
 
 import App from './App';
 
@@ -20,4 +19,3 @@ ReactDOM.render(
   </>,
   document.getElementById('root')
 );
-resso.config({ batch: ReactDOM.unstable_batchedUpdates });
